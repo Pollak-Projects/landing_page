beb84698f822d29550e88f4546a270bdf5eedf9f
Huszka Adrián Gábor <adrian.huszka@gmail.com>
2024-09-19 14:00:17 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Card from './components/Card.vue'
+</script>
+
+<template>
+  <main class="h-screen w-screen bg-gradient-to-bl from-[#5585b5] via-[#bbe4e9] p-10">
+    <h1 class="text-[3rem] h-[150px] flex items-center justify-center">Landing Page</h1>
+    <div class="h-[calc(100%-150px)] w-full gap-5 flex flex-col justify-center items-center md:flex-row md:flex-wrap">
+      <Card title="Időpontfoglaló" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry." to="" class="bg-[url(https://i0.wp.com/blog.ontheballpromotions.com/wp-content/uploads/2017/09/financial-planner-reference-calendar.jpg?fit=1200%2C800&ssl=1)]" />
+      <Card title="Büfé" description="Pollák büfé" to="" class="bg-[url(https://pollakbufe.hu/public/Images/bufe.jpg)]" />
+      <Card title="Büfé" description="Pollák büfé" to=""/>
+      <Card title="Büfé" description="Pollák büfé" to="" />
+      <Card title="Büfé" description="Pollák büfé" to="" />
+      <Card title="Büfé" description="Pollák büfé" to="" />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  title: String,
+  description: String,
+  to: String,
+  bg: String,
+})
+
+</script>
+
+<template>
+  <div class="md:w-1/4 h-48 w-full bg-white rounded-xl border-2 border-black bg-cover cursor-pointer">
+    <div id="div" class="h-full w-full backdrop-blur-sm p-2">
+      <h1 class="text-[3rem] text-center text-white">{{ title }}</h1>
+      <p class="text-lg text-white text-ellipsis">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  #div {
+    overflow: hidden;
+  }
+
+  #div::before {
+    content: 'Tovább';
+    border-radius: 0.75rem;
+    font-size: 3rem;
+    align-items: center;
+    display: none;
+    position: absolute;
+    justify-content: center;
+    align-items: center;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #00000096;
+    backdrop-filter: blur(10px);
+    z-index: 1;
+  }
+
+  #div::after {
+    content: 'asd';
+    border-radius: 0.75rem;
+    font-size: 3rem;
+    align-items: center;
+    display: none;
+    position: absolute;
+    justify-content: center;
+    align-items: center;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #00000096;
+    backdrop-filter: blur(10px);
+    z-index: 2;
+  }
+
+  #div:hover::before {
+    animation: slide 1s forwards;
+    display: flex;
+  }
+
+  @keyframes slide {
+    0% {
+      transform: translateX(-100%);
+    }
+    100% {
+      transform: translateX(0%);
+    }
+  }
+</style>
