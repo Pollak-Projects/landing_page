35a8423d98b12097a50c8364d042bb33f349448c
Huszka Adrian <adrian.huszka@gmail.com>
2024-09-20 20:26:50 +0200

    Added webhook for autoupdate

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,45 @@
 <script setup>
-import Card from './components/Card.vue'
+import Card from "./components/Card.vue";
 </script>
 
 <template>
-  <main class="h-screen w-screen bg-[url('./assets/fullhatter.jpg')] p-10">
-    <h1 class="text-[3rem] h-[150px] flex items-center justify-center">Landing Page</h1>
-    <div class="h-[calc(100%-150px)] w-full gap-5 flex flex-col justify-center items-center md:flex-row md:flex-wrap">
-      <Card title="Időpontfoglaló" description="" to="" class="bg-[url(https://i0.wp.com/blog.ontheballpromotions.com/wp-content/uploads/2017/09/financial-planner-reference-calendar.jpg?fit=1200%2C800&ssl=1)]" />
-      <Card title="Büfé" description="" to="" class="bg-[url(https://pollakbufe.hu/public/Images/bufe.jpg)]" />
-      <Card title="Selfie automata" description="" to="" class="bg-[url(https://www.selfieparty.hu/wp-content/uploads/selfiegep-eskuvore-1.jpg)]"/>
-      <Card title="PollákMix" description="" to="" class="bg-[url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKb6ZTMRUHc788L_9Kfzt7h6HiUV7X4-jPhQ&s)]"/>
-      <Card title="" description="" to="" />
-      <Card title="Büfé" description="" to="" />
+  <main
+    class="min-h-screen w-screen bg-[url('./assets/fullhatter.jpg')] bg-no-repeat bg-cover p-10 overflow-y-auto"
+  >
+    <h1 class="text-[3rem] h-[150px] flex items-center justify-center">
+      Landing Page
+    </h1>
+    <div
+      class="h-[calc(100%-150px)] w-full gap-5 flex flex-col justify-center items-center md:flex-row md:flex-wrap"
+    >
+      <Card
+        title="Időpontfoglaló"
+        description=""
+        to=""
+        class="bg-[url(https://i0.wp.com/blog.ontheballpromotions.com/wp-content/uploads/2017/09/financial-planner-reference-calendar.jpg?fit=1200%2C800&ssl=1)]"
+      />
+      <Card
+        title="Büfé"
+        description=""
+        to=""
+        class="bg-[url(https://pollakbufe.hu/public/Images/bufe.jpg)]"
+      />
+      <Card
+        title="Selfie automata"
+        description=""
+        to=""
+        class="bg-[url(https://www.selfieparty.hu/wp-content/uploads/selfiegep-eskuvore-1.jpg)]"
+      />
+      <Card
+        title="PollákMix"
+        description=""
+        to=""
+        class="bg-[url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKb6ZTMRUHc788L_9Kfzt7h6HiUV7X4-jPhQ&s)]"
+      />
+      <!-- <Card title="" description="" to="" />
+      <Card title="Büfé" description="" to="" /> -->
     </div>
   </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,62 +1,65 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 defineProps({
   title: String,
   description: String,
   to: String,
   bg: String,
-})
-
+});
 </script>
 
 <template>
-  <div class="md:w-1/4 h-48 w-full bg-white rounded-xl border-2 border-black bg-cover cursor-pointer">
-    <div id="div" class="h-full w-full backdrop-blur-sm p-2">
-      <h1 class="text-[3rem] text-center text-white">{{ title }}</h1>
+  <div
+    class="lg:min-w-96 lg:w-1/4 h-48 lg:h-96 w-full bg-white rounded-xl border-2 border-black bg-cover cursor-pointer"
+  >
+    <div id="div" class="h-full w-full backdrop-blur-sm p-2 rounded-xl">
+      <h1 class="text-[2rem] text-center text-white md:text-[3rem]">
+        {{ title }}
+      </h1>
       <p class="text-lg text-white text-ellipsis">{{ description }}</p>
     </div>
   </div>
 </template>
 
 <style scoped>
-  #div {
-    overflow: hidden;
-  }
+#div {
+  overflow: hidden;
+}
 
-  #div::before {
-    content: '';
-    background-image: url('../assets/right-arrow-svgrepo-com.svg');
-    background-size:  180px 180px ;
-    background-repeat: no-repeat;
-    background-position: center;
-    color: rgb(226, 78, 78);
-    border-radius: 0.75rem;
-    font-size: 3rem;
-    align-items: center;
-    display: none;
-    position: absolute;
-    justify-content: center;
-    align-items: center;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: #000000dc;
-    backdrop-filter: blur(10px);
-    z-index: 1;
-  }
-  #div:hover::before {
-    animation: slide 1s forwards;
-    display: flex;
-  }
+#div::before {
+  content: "";
+  background-image: url("../assets/right-arrow-svgrepo-com.svg");
+  background-size: 180px 180px;
+  background-repeat: no-repeat;
+  background-position: center;
+  color: rgb(226, 78, 78);
+  border-radius: 0.75rem;
+  font-size: 3rem;
+  align-items: center;
+  display: none;
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #000000dc;
+  backdrop-filter: blur(10px);
+  z-index: 1;
+}
+#div:hover::before {
+  animation: slide 1s forwards;
+  display: flex;
+}
 
-  @keyframes slide {
-    0% {
-      transform: translateX(-100%);
-    }
-    100% {
-      transform: translateX(0%);
-    }
+@keyframes slide {
+  0% {
+    transform: translateX(-100%);
   }
+  100% {
+    transform: translateX(0%);
+  }
+}
 </style>
