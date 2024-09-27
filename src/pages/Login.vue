97c54be45921094f9ee7f68f6cc85aecf6b0383b
CristinaiRunaldo <106381446+CristinaiRunaldo@users.noreply.github.com>
2024-09-27 10:13:27 +0000

    login design part2

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,12 +1,12 @@
 <script setup></script>
 <template>
   <div class="h-full w-full flex flex-col justify-center items-center">
-    <div class="flex bg-slate-500 h-40 w-1/5 justify-center items-center rounded-2xl">
-        <div class="flex flex-col gap-2 w-4/5">
+    <div class="flex bg-slate-500 h-60 w-1/5 justify-center items-center rounded-2xl">
+        <div class="flex flex-col gap-4 w-4/5">
           <h1 class="text-gray-50 font-semibold text-3xl">Bejelentkezés</h1>
-          <input type="text" placeholder="E-mail" class="rounded-lg text-balance"/>
-          <input type="password" placeholder="Jelszó" class="rounded-lg"/>
-          <input type="submit" value="Bejelentkezés" class="bg-white w-1/3 justify-center items-center flex rounded-lg cursor-pointer">
+          <input type="text" placeholder="E-mail" class="rounded-xl text-balance mt-4 text-center mb-2 h-8"/>
+          <input type="password" placeholder="Jelszó" class="rounded-xl h-8 text-center"/>
+          <input type="submit" value="Bejelentkezés" class="justify-center items-center flex rounded-xl cursor-pointer mt-1 ml-24 bg-gray-600 text-slate-200 font-semibold w-2/5 h-8">
       </div>
     </div>
   </div>
