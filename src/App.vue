46de9c909fbc454d2cc652097fdaffa6ace89a95
CristinaiRunaldo <106381446+CristinaiRunaldo@users.noreply.github.com>
2024-10-01 14:09:27 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import { RouterView } from "vue-router";
   
   </main>
   <footer
-    class="bg-slate-800 text-slate-100 fixed bottom-0 w-screen flex items-center justify-center md:h-7"
+    class="bg-slate-800 text-slate-100 fixed bottom-0 w-screen flex items-center justify-center md:h-7 text-center"
   >
     Készítette: 12. évfolyam Szoftverfejlesztők © Copyright 2024-2025
   </footer>
